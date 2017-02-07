9215dc782b03482f9a80bc81f8d314342e6163cd
[01;31m[K"name"[m[K: "Felix Vandelanotte <[01;31m[K"email"[m[K: "vandelanottefelix@gmail.com>
2017-02-07 15:26:19 +0100

    add comments

--- game.rb
+++ game.rb
@@ -13,13 +13,15 @@ class Game
       bord.print_bord
 
       play(current_player)
+      #stop playing if there is a winner and print out a winning message
       break if self.check_winner(current_player) == true
+      #switch players after every move
       if current_player == player1
         current_player = player2
       else
         current_player = player1
       end
-
+      #tie happens when all of the moves have been taken and no winner has been declared
       if i == 9
         puts "Tie, nobody won"
       end
@@ -36,12 +38,14 @@ class Game
     puts "player #{current_player.name}: make your move"
 
     move = gets.chomp.to_i
-
+    #if user input is correct and the move is still empty
     if bord.values_bord[move] == " "
       bord.values_bord[move] = current_player.name
+      #if user input is wrong
     elsif  !(1..9).include?(move)
       puts "Wrong input, please pick a number from 1 to 9"
       self.play(current_player)
+      #if user input is allready taken
     else
       puts "Already taken, play again"
       puts "\n"
